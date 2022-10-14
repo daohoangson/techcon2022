30dc6d3c0651b96a39179ad27f5e701ef24ab74e
Dao Hoang Son <daohoangson@gmail.com>
2022-10-14 14:08:41 +0700

    Configure Amplify

--- lib/src/app_root.dart
+++ lib/src/app_root.dart
@@ -1,10 +1,33 @@
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:material_color_generator/material_color_generator.dart';
 
+import '../amplifyconfiguration.dart';
 import 'app_screen.dart';
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    _configureAmplify();
+  }
+
+  Future<void> _configureAmplify() async {
+    try {
+      await Amplify.addPlugin(AmplifyAuthCognito());
+      await Amplify.configure(amplifyconfig);
+    } on Exception catch (e) {
+      debugPrint('Could not configure Amplify: $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
