7b7dccf6af67884a66cb36cc77beab8c46ea5000
Dao Hoang Son <daohoangson@gmail.com>
2022-10-16 00:12:34 +0700

    `amplify init --appId d1sn7zu80v9gh7`

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,12 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "techcon2022api": {
+            "ServiceName": "string",
+            "ClusterName": "string",
+            "PipelineName": "string",
+            "ContainerNames": "string",
+            "ApiName": "string",
+            "RootUrl": "string"
+        }
+    }
+}
