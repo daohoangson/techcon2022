daf8269ef15aacdeb49555ba26ce187fd22f049e
Dao Hoang Son <daohoangson@gmail.com>
2022-10-16 16:38:11 +0700

    Replace `Amplify.API` with `http` package due to missing Web support

--- lib/src/api_client.dart
+++ lib/src/api_client.dart
@@ -1,12 +1,12 @@
 import 'dart:convert';
 
 import 'package:amplify_flutter/amplify_flutter.dart';
-import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 
 class ApiClient {
   Future<int> getValue() async {
-    const opts = RestOptions(path: '/');
-    final resp = await Amplify.API.get(restOptions: opts).response;
+    final endpoint = await _getEndpoint();
+    final resp = await http.get(Uri.parse(endpoint));
     final value = int.tryParse(resp.body) ?? 0;
     return value;
   }
@@ -14,12 +14,33 @@ class ApiClient {
   Future<int> increaseValue() async {
     final user = await Amplify.Auth.getCurrentUser();
     final body = jsonEncode(<String, String>{'user_id': user.userId});
-    final opts = RestOptions(
-      path: '/',
-      body: Uint8List.fromList(body.codeUnits),
-    );
-    final resp = await Amplify.API.post(restOptions: opts).response;
+    final endpoint = await _getEndpoint();
+    final resp = await http.post(Uri.parse(endpoint), body: body);
     final value = int.tryParse(resp.body) ?? 0;
     return value;
+  }
+
+  Future<String> _getEndpoint() async {
+    final amplifyConfig = await Amplify.asyncConfig;
+    final Map<String, AmplifyPluginConfig> apiPlugins =
+        amplifyConfig.api?.plugins ?? {};
+    for (final entry in apiPlugins.entries) {
+      final apiConfig = entry.value.toJson()['techcon2022api'];
+      if (apiConfig is! Map) {
+        continue;
+      }
+
+      final endpoint = apiConfig['endpoint'];
+      if (endpoint is! String) {
+        continue;
+      }
+
+      return endpoint;
+    }
+
+    throw Exception(
+      'Unable to determine endpoint from '
+      'amplifyConfig=${amplifyConfig.toJson()}',
+    );
   }
 }
